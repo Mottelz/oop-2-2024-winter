1420884deaee410e0ee346b6ce02545173aa8c36
Mottel <shachna@gmail.com>
2024-03-10 11:13:22 -0400

    Added 404 and error handlers

--- example-express/index.js
+++ example-express/index.js
@@ -14,7 +14,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
     res.send('Please come back later.');
 });
+app.get('/error', (req, res) => {
+    throw new Error('Something has gone horribly worng. Please take a nap as soon as possible.')
+});
 app.use('/patient', patientRouter);
+
+// 404 & Error Handling
+app.all('*', (req, res) => {
+    res.status(404).json({
+        msg: 'Something was wrong with your request', 
+        reqMethod: req.method,
+        reqPath: req.path,
+        reqQuery: req.query,
+        reqBody: req.body, 
+    });
+});
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        msg: 'Internal Server Error',
+    });
+};
+app.use(errorHandler);
 
 // Set Server to Listen
 app.listen(port, () => {
