f2f4233d477a3425fc1994d64ab4fd253c5192f0
Mottel <shachna@gmail.com>
2024-04-21 11:44:45 -0400

    Added fail test to players/create

--- midtournament/tests/e2e/routers/player.spec.js
+++ midtournament/tests/e2e/routers/player.spec.js
@@ -14,12 +14,24 @@ describe('Player routes', () => {
                 email: 'fool@test.com',
                 discord: 'foolish_dummy'
             }
+
             const response = await request(app).post('/players/create').send(dummy);
 
             const dbPlayer = await utils.getRecordFromDB('Players', 'email', dummy.email);
 
             expect(response.statusCode).toBe(200);
             expect(dbPlayer).toMatchObject(dummy);
-        })
-    })
+        });
+
+        test('should respond 400 if missing discord', async ()  => {
+            const dummy = {
+                name: 'test dummy',
+                email: 'test@dummy.com'
+            };
+
+            const response = await request(app).post('/players/create').send(dummy);
+
+            expect(response.statusCode).toBe(400);
+        });
+    });
 });
--- midtournament/tests/utils.js
+++ midtournament/tests/utils.js
@@ -1,16 +1,17 @@
 const db = require('../src/data/init');
+
 async function initializeDB () {
     await db.init();
 };
 
 async function getRecordFromDB(table, field, value) {
-    const raw = `SELECT rowid, * from ${table} WHERE ${field}=?`;
+    const raw = `SELECT rowid, * FROM ${table} WHERE ${field}=?`;
     const stmnt = db.db.prepare(raw);
     const data = await stmnt.get(value);
     return data;
-}
+};
 
 module.exports = {
     initializeDB,
     getRecordFromDB
-}
+};
